e7b71e00b5703017df5739a02e61a9444c680563
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2019-03-13 22:15:24 -0400

    added txt exception

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 readDir = util.promisify(fs.readdir);
 readFile = util.promisify(fs.readFile);
 
-let allFiles = readDir(path.resolve(__dirname, "Contracts", "Fiscal\ 2018"));
+let allFiles = readDir(path.resolve(__dirname, "Contracts"));
 
 allFiles
     .then(async(files) => {
         let texts = [];
         for(const file of files){
-            let contents = await readFile(path.resolve(__dirname, "Contracts", "Fiscal\ 2018", file));
+            if(file.substring(file.length - 3, file.length) == "txt"){ // If text file...
+            let contents = await readFile(path.resolve(__dirname, "Contracts", file));
             texts.push({ date: file.substring(0, file.length - 4), content: contents.toString()} );
+            }
         };
         return texts;
     })
@@ -23,9 +25,15 @@ allFiles
         texts.forEach(({ content, date }) => {
             // let contracts = content.split('\n');
             let Lockheed = content.match(/BAE Systems/g)
+            let n = content.match(/\$([0-9.,]+)/) // Find value of contract (must build failsafe if contract value is undefined...)
+            let n2 = n[1].replace(/[,.]/g, "");
+            nInt = parseInt(n2);
+
+
             if(Lockheed){
                 total = total + Lockheed.length;
+                console.log(nInt);
             }
         });
-        console.log(total);
+        // console.log(total);
     });
